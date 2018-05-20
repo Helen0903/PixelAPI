2c3caea0beae363a7fe9391d116b0b86fbe7fda0
Helen <helen0903brown@gmail.com>
2018-05-20 15:56:47 +0100

    (Grid) Update test
    -To reflect changes in the class

--- tests/GridTest.java
+++ tests/GridTest.java
@@ -7,6 +7,6 @@ import pixel.GridSetting;
 public class GridTest {
 	public static void main(String[] args) {
 		Grid grid = new Grid(new GridSetting(500, 500, 50, 50));
-		grid.setRect(new Rectangle(0, 0, 50, 50), new Color(255, 255, 255));
+		grid.setRect(new Rectangle(1, 1, 20, 20), new Color(255, 255, 255));
 	}
 }
